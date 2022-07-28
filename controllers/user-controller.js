cb605bc16d09b47aa9cd978072eeb1cade1d8b44
keeezy <lazarokev@gmail.com>
2022-07-28 01:07:55 -0700

    modified user-controller, added post/put/delete to create,update, delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -23,7 +23,70 @@ module.exports = {
         } catch (err) {
             res.json({ message: 'Error getting single user!!! ', err });
         }
+    },
+
+    // POST /api/users
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.json({ message: 'Error creating user!!!', err });
+        }
+    },
+
+    // PUT /api/users/:id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate({_id: req.params.id}, {$set: req.body}, {new: true});
+            if (!user) {
+                return res.json({ message: 'No updated user found!!!', err });
+            }
+            res.json(user);
+        } catch (err) {
+            res.json({ message: 'Error updating user!!!', err });
+        }
+    },
+
+    // DELETE /api/users/:id
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({_id: req.params.id});
+            if (!user) {
+                return res.json({ message: 'No user found with this ID!!!', err });
+            }
+            res.json(user);
+        } catch (err) {
+            res.json({ message: 'Error deleting user!!!', err });
+        }
+    },
+
+    // POST /api/users/:userID/friends/:friendID
+    async addFriend(req, res) {
+        try {
+            const user = await User.findIdAndUpdate({_id: req.params.userID}, {$push: {friends: req.params.friendID}});
+            if (!user) {
+                return res.json({ message: 'No user found with this ID!!!', err });
+            }
+            res.json(user);
+        } catch (err) {
+            res.json({ message: 'Error adding friend!!!', err });
+        }
+    },
+
+    // DELETE /api/users/:userID/friends/:friendID
+    async removeFriend(req, res) {
+        try {
+            const user = await User.findIdAndUpdate({_id: req.params.userID}, {$pull: {friends: req.params.friendID}});
+            if (!user) {
+                return res.json({ message: 'No user found with this ID!!!', err });
+            }
+            res.json(user);
+        } catch (err) {
+            res.json({ message: 'Error removing friend!!!', err });
+        }
     }
+
 
 
 
